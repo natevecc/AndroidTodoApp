859821ac45674465fea3b72766837943c21717e7
Nathan Vecchiarelli <natevecc@gmail.com>
2014-07-09 22:14:37 -0400

    added simple list view

--- app/src/main/java/com/example/natevecc/todoapp/TodoList.java
+++ app/src/main/java/com/example/natevecc/todoapp/TodoList.java
@@ -4,14 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class TodoList extends Activity {
+
+    private ListView todoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+        this.todoListView = (ListView) findViewById(R.id.todoList);
+
+        String[] values = new String[] {
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        this.todoListView.setAdapter(adapter);
     }
 
 
